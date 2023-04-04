9fdca1b19070f01fcc8454d3872047fb2a7ffa56
Cameron McClure-Coleman <cameronmcclure-coleman@Camerons-MacBook-Pro-9.local>
2023-04-04 14:33:58 -0700

    added telemtry_packet struct header file and states enum header file with basic structure for both

---
+++ src/states.h
@@ -0,0 +1,12 @@
+#ifndef STATES_H_
+#define STATES_H_
+enum mission_state {
+    NAV_CONVERGE,
+    PAD_IDLE,
+    ASCENT,
+    APOGEE,
+    DESCENT,
+    LANDED
+};
+
+#endif //STATES_H_
---
+++ src/telemetry_packet.h
@@ -0,0 +1,29 @@
+/*
+    * Telemetry Packet
+    * 
+    * This file contains the class definition for the Telemetry Packet class.
+    * This class is used to create a packet of data to be sent to the ground.
+    * Data included in the packet is the following:
+    * - a timestamp
+    * - current state
+    * - most recent IMU poll
+    * - most recent altimeter poll
+    * - most recent GPS poll
+    * Author: Cameron McClure-Coleman
+*/
+
+#ifndef TELEMETRY_PACKET_H_
+#define TELEMETRY_PACKET_H_
+
+#include <Arduino.h>
+#include "states.h"
+
+struct telemetry_packet{
+    uint32_t timestamp;
+    mission_state state;
+    uint8_t imu_data[6];
+    uint8_t altimeter_data[4];
+    uint8_t gps_data[4];
+} telemetry_packet;
+
+#endif // TELEMETRY_PACKET_H_
