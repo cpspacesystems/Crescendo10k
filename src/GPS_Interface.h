0f7b92ef8aae19f92707a525b6181c23f1ca0c91
Saul <saulargz35@gmail.com>
2023-04-09 22:02:44 -0700

    added get_pos method

--- src/GPS_Interface.h
+++ src/GPS_Interface.h
@@ -1,1 +1,24 @@
 #include <Arduino.h>
+#include "TinyGPS++.h"
+
+union POS {double lat; double lng;}; //position union
+
+class GroveGPS{
+
+    public:
+
+    TinyGPSPlus gps;
+    POS pos;
+    
+
+    POS get_pos(){
+
+        GroveGPS::pos.lat = gps.location.lat();
+        GroveGPS::pos.lng = gps.location.lng();
+
+        return pos;
+    }
+
+};
+
+
