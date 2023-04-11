5d696b1922b743ef503a8034af1547ddd0b2f096
Cameron McClure-Coleman <cameronmcclure-coleman@Camerons-MacBook-Pro-9.local>
2023-04-10 19:33:10 -0700

    test gps reception 1

--- src/GPS_Interface.cpp
+++ src/GPS_Interface.cpp
@@ -1,47 +1,43 @@
 #include <Arduino.h>
 #include "TinyGPS++.h"
+#include "SoftwareSerial.h"
 
+
+SoftwareSerial ss(22, 21);
 union POS {double lat; double lng;}; //position union
 
 class GroveGPS{
-
-    public:
+    private:
     POS latest_pos;
     TinyGPSPlus gps;
+    public:
     
     GroveGPS(){
         latest_pos.lat = 0;
         latest_pos.lng = 0;
         gps = TinyGPSPlus();
     }
-    
-    
-
 
     POS get_pos(){
 
         latest_pos.lat = gps.location.lat();
         latest_pos.lng = gps.location.lng();
-
         return latest_pos;
     }
 
     double get_lat(){
-
-        latest_pos.lat = gps.location.lat();
-
         return latest_pos.lat;
     }
 
     double get_lng(){
-
-        latest_pos.lng = gps.location.lng();
-
         return latest_pos.lng;
     }
 
     void update_pos(){
+            while (ss.available() > 0){
+            gps.encode(ss.read());
             latest_pos.lat = gps.location.lat();
             latest_pos.lng = gps.location.lng();
+        }
     }
 };
