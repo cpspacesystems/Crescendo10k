ab6aa7d43630e8ce89b21fd72d5d055690ed0179
Cameron McClure-Coleman <cameronmcclure-coleman@Camerons-MacBook-Pro-9.local>
2023-04-15 00:14:46 -0700

    test code in place need to figure out why gps returns 0 for lat and lng

--- src/main.cpp
+++ src/main.cpp
@@ -6,17 +6,17 @@ GroveGPS gps;
 void setup() 
 {
     pinMode(LED_BUILTIN, OUTPUT);
-    pinMode(21, OUTPUT);
-    pinMode(22, INPUT);
-    Serial.begin(9600);
+    pinMode(21, OUTPUT); //GPS TX
+    pinMode(22, INPUT);  //GPS RX
+    // Serial.begin(9600);
 
     gps = GroveGPS();
-    
+    gps.update_pos();
 }
 
 
 void loop()
 {
-    POS pos = gps.get_pos();
-    Serial.printf("Lat: %f, Lng: %f\n", pos.lat, pos.lng);
+    // POS pos = gps.get_pos();
+    // Serial.printf("Lat: %f, Lng: %f\n", pos.lat, pos.lng);
 }
