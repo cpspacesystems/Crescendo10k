68e4d32df958026bc1129848fb79759bdfddcbab
spagdoon0411 <spandan.suthar20042011@gmail.com>
2023-04-15 18:07:49 -0700

    cleared main.cpp of testing code

--- src/main.cpp
+++ src/main.cpp
@@ -3,32 +3,21 @@
 #include "states.h"
 
 static telemetry_packet packet = {
-    100, 
-    PAD_IDLE, 
-    {0, 1, 2, 3, 4, 5},
-    {6, 7, 8, 9},
-    {10, 11, 12, 13}
+    0, 
+    (mission_state)6, 
+    {0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0},
+    {0, 0, 0, 0}
 };
 
 static SD_Interface *sDLog;
 
 void setup() 
 {
-    Serial.begin(9600);
-    pinMode(2, OUTPUT);
 
-    sDLog = new SD_Interface(&packet);
-
-    sDLog->logPacket();
-
-    sDLog->close();
 }
-
 
 void loop()
 {
-    digitalWrite(2, LOW);
-    delay(1000);
-    digitalWrite(2, HIGH);
-    delay(1000);
+
 }
